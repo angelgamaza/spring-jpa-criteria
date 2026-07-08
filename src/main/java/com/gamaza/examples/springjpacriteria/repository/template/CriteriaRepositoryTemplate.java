6384fd19d164a05fda5320376c29a03439a32b14
angelgamaza <angel.gamaza@gmail.com>
2026-07-08 19:32:48 +0200

    Minor fixes

--- src/main/java/com/gamaza/examples/springjpacriteria/repository/template/CriteriaRepositoryTemplate.java
+++ src/main/java/com/gamaza/examples/springjpacriteria/repository/template/CriteriaRepositoryTemplate.java
@@ -14,10 +14,8 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.lang.reflect.ParameterizedType;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.lang.Boolean.TRUE;
